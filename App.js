b6183a16b56aa4b18fcb7d51cb4a9c77a9b4025a
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2021-04-17 10:27:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
-import WriteStoryScreen from './screens/WriteStoryScreen'
-import ReadStoryScreen from './screens/ReadStoryScreen'
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import LoginScreen from './screens/LoginScreen';
+import WriteScreen from './screens/WriteScreen';
+import ReadScreen from './screens/ReadScreen';
 
 export default class App extends React.Component {
-  render(){
-    return(
-      <AppContainer />
-    );
+  render() {
+    return <AppContainer />;
   }
 }
 
-const TabNavigator = createBottomTabNavigator({
-  WriteStory: WriteStoryScreen,
-  ReadStory: ReadStoryScreen
+const TabNavigator = createMaterialBottomTabNavigator({
+  Read: { screen: ReadScreen },
+  Write: { screen: WriteScreen },
+},
+{
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: () => {
+      const routeName = navigation.state.routeName; console.log(routeName)
+      if (routeName === "Read") {
+        return (<Image source={require("./assets/read.png")} 
+        style={{ width: 40, height: 40 }} />)
+      }
+      else if (routeName === "Write") {
+        return (<Image source={
+          require("./assets/write.png")
+        } style={{ width: 40, height: 40 }} />)
+      }
+    }
+  })
 }
-  )
+);
 
-const AppContainer = createAppContainer(TabNavigator);
+const AppNavigator = createSwitchNavigator({
+  LoginScreen: {screen : LoginScreen},
+  TabNavigator : {screen : TabNavigator}
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppContainer = createAppContainer(AppNavigator);
--- config.js
+++ config.js
@@ -1,21 +1,16 @@
-import firebase from 'firebase'
-require("@firebase/firestore")
-
-
-// Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyAMW87I3aehUsTmQ-LXmNvxw8QZ1e0Xwto",
-    authDomain: "bed-time-stories-805e9.firebaseapp.com",
-    databaseURL: "https://bed-time-stories-805e9.firebaseio.com",
-    projectId: "bed-time-stories-805e9",
-    storageBucket: "bed-time-stories-805e9.appspot.com",
-    messagingSenderId: "74595764582",
-    appId: "1:74595764582:web:9a930873bddda1e548e0a7",
-    measurementId: "G-1X269F7P47"
+import *as firebase from "firebase"
+require('@firebase/firestore')
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyDvoFCWi44DmPl1dv3TTSxOExRVTszzvKM",
+    authDomain: "story-hub-f2fc0.firebaseapp.com",
+    databaseURL: "https://story-hub-f2fc0.firebaseio.com",
+    projectId: "story-hub-f2fc0",
+    storageBucket: "story-hub-f2fc0.appspot.com",
+    messagingSenderId: "876369744834",
+    appId: "1:876369744834:web:a885a932b01a09d4d7fc5b"
   };
+  
+  firebase.initializeApp(firebaseConfig);
 
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export default  firebase.firestore()                                   
+  export default firebase.firestore();
